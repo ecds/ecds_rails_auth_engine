b4a8333efcc429029ec703011cb97182e2644174
Jay Varner <jayvarner@gmail.com>
2021-07-30 10:33:25 -0400

    Clean out some junk

--- app/controllers/ecds_rails_auth_engine/current_user.rb
+++ app/controllers/ecds_rails_auth_engine/current_user.rb
@@ -13,8 +13,6 @@ module EcdsRailsAuthEngine
       # @return [User] <description>
       #
       def current_user
-        # a = request.headers['Authorization']
-        # 10.times { puts headers }
         token = if Rails.env == 'test'
           cookies[:auth]
         else
@@ -22,20 +20,12 @@ module EcdsRailsAuthEngine
         end
 
         return User.new if token.nil?
-        # begin
-        #   token = a.split(' ').last
 
-        #   return nil if token == 'undefined'
         login = EcdsRailsAuthEngine::Login.find_by(token: token)
-        return User.new if login.nil?
-        # return { cookie: token, signed: cookies.signed[:auth]}
 
-        # return nil unless TokenService.verify(login.token)
+        return User.new if login.nil?
 
         User.find(login.user_id)
-        # rescue NoMethodError
-        #   nil
-        # end
       end
     end
   end
